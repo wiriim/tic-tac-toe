f5117363bcc8ff399e6ad913c031f5c480d01cbf
William Halim <51820876+wiriim@users.noreply.github.com>
2025-05-14 22:21:23 +0700

    Add working ui on click events

--- index.js
+++ index.js
@@ -37,6 +37,13 @@ let gameBoard = (function(){
         }
     }
 
+    function resetBoard(){
+        for (let i = 0; i < 3; i++){
+            board[i].fill(null);
+        }
+        console.log(board);
+    }
+
     function isGameOver(){
         if ((board[0][0] == board[1][1]) && (board[1][1] == board[2][2]) && board[1][1] != null) return board[1][1] + " Wins";
         if ((board[2][0] == board[1][1]) && (board[1][1] == board[0][2]) && board[1][1] != null) return board[1][1] + " Wins";
@@ -44,30 +51,76 @@ let gameBoard = (function(){
             if ((board[i][0] == board[i][1]) && (board[i][1] == board[i][2]) && board[i][1] != null) return board[i][1] + " Wins";
             if ((board[0][i] == board[1][i]) && (board[1][i] == board[2][i]) && board[1][i] != null) return board[1][i] + " Wins";
         }
-        if (!board.includes(null)) return "Draw";
-        return false
+        if (board[0].every(element => element !== null) && 
+        board[1].every(element => element !== null) && 
+        board[2].every(element => element !== null)) return "Draw";
+        return false;
     }
     
-    return {board, setCircle, setCross, setRandomCircle, setRandomCross, isGameOver};
+    return {board, setCircle, setCross, setRandomCircle, setRandomCross, resetBoard,isGameOver};
 })();
 
 let crossContainer = document.querySelector(".cross-container");
 let circleContainer = document.querySelector(".circle-container");
-let player;
+let player = "X";
 
 crossContainer.addEventListener("click", ()=>{
     crossContainer.dataset.active = "true";
     circleContainer.dataset.active = "false";
-    crossContainer.style.backgroundColor = "var(--lightGrey)"
-    circleContainer.style.backgroundColor = "var(--lightBlue)"
+    crossContainer.style.backgroundColor = "var(--lightGrey)";
+    circleContainer.style.backgroundColor = "var(--lightBlue)";
+    player = "X";
+    gameBoard.resetBoard();
+    drawBoard();
 });
 
 circleContainer.addEventListener("click", ()=>{
     circleContainer.dataset.active = "true";
     crossContainer.dataset.active = "false";
-    circleContainer.style.backgroundColor = "var(--lightGrey)"
-    crossContainer.style.backgroundColor = "var(--lightBlue)"
+    circleContainer.style.backgroundColor = "var(--lightGrey)";
+    crossContainer.style.backgroundColor = "var(--lightBlue)";
+    player = "O";
+    gameBoard.resetBoard();
+    gameBoard.setRandomCross();
+    drawBoard();
 });
 
-crossContainer.dataset.active == "true" ? player = "X" : player = "O";
-console.log(player)
+let blocks = document.querySelectorAll(".block");
+
+blocks.forEach(block => {
+    block.addEventListener("click", e =>{
+        let msg = "";
+        if (gameBoard.isGameOver() == false){
+            if (player == "X"){
+                msg = gameBoard.setCross(e.target.dataset.row, e.target.dataset.col);
+                if (msg == "Cross Placed" && gameBoard.isGameOver() == false) gameBoard.setRandomCircle();
+            }
+            else if (player == "O"){
+                msg = gameBoard.setCircle(e.target.dataset.row, e.target.dataset.col);
+                if (msg == "Circle Placed" && gameBoard.isGameOver() == false) gameBoard.setRandomCross();
+            }
+            drawBoard();
+        }
+        console.log(gameBoard.board)
+        console.log(gameBoard.isGameOver());
+    });
+});
+
+function drawBoard(){
+    let block;
+    for (let i = 0; i < 3; i++){
+        for (let j = 0; j < 3; j++){
+            if (gameBoard.board[i][j] != null){
+                block = document.querySelector(`[data-row="${i}"][data-col="${j}"]`);
+                block.innerHTML = "";
+                let drawing = document.createElement("div");
+                gameBoard.board[i][j] == "X" ? drawing.classList.add("cross") : drawing.classList.add("circle");
+                block.appendChild(drawing);
+            }
+            else{
+                block = document.querySelector(`[data-row="${i}"][data-col="${j}"]`);
+                if (block.firstChild) block.removeChild(block.firstChild);
+            }
+        }
+    }
+}
